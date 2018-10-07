beea3b6ae675802950c7acf7e0dab95883e1d594
oldmalebird <oldmalebird@users.noreply.github.com>
2018-10-08 00:23:32 +0800

    R histogram

--- lesson2/reddit.R
+++ lesson2/reddit.R
@@ -3,3 +3,13 @@ reddit
 str(reddit)
 table(reddit$employment.status)
 summary(reddit)
+levels(reddit$age.range)
+library(ggplot2)
+qplot(data = reddit, x = age.range)
+qqplot(data = reddit, x = income.range)
+
+#setting levels of ordered factors solution
+reddit$age.range <- ordered(reddit$age.range, levels = c('Under 18', '18-24','25-34','35-44','45-54','55-64','65 or Above'))
+
+#alternate solution
+reddit$age.range <- factor(reddit$age.range, levels = c('Under 18', '18-24','25-34','35-44','45-54','55-64','65 or Above'), ordered = T)
